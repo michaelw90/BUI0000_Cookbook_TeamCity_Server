78dbf6fd98322f1f9a873a98ce4f0b9d7ebce5f1
Fred Thompson <fred.thompson@buildempire.co.uk>
2015-03-14 09:48:32 +0000

    Added the azure plugin.

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -16,3 +16,11 @@ cookbook_file "#{data_plugins_directory}/teamcity.github.zip" do
   source "teamcity.github.zip"
   action :create_if_missing
 end
+
+# Install the azure cloud agent plugin.
+# This enables cloud agent facilities from Azure VMs
+cookbook_file "#{data_plugins_directory}/azure-cloud.zip" do
+  backup false
+  source "azure-cloud.zip"
+  action :create_if_missing
+end
